3f00aa5f9ff0ad00880c056d045e2f15fb9d34b0
Hal Carleton <hello@halleycarleton.com>
2016-11-07 12:09:15 -0600

    adding completed stuff

---
+++ bash.js
@@ -0,0 +1,21 @@
+'usen strict';
+
+var command = require('./lib/commands');
+
+// Output a prompt
+process.stdout.write('prompt > ');
+
+// The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', function (data) {
+  const args = data.toString().trim().split(/\s+/g); // remove the newline
+  const cmd  = args.shift();
+  const cb = function() {
+    process.stdout.write('\nprompt > ');
+  };
+
+  if(command[cmd]){ command[cmd].apply(undefined, args.concat([cb])); }
+  else{
+    process.stdout.write('cmd doesn\'t exist');
+    cb();
+  }
+});
